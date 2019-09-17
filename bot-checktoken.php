6f5ab94f685e3c1eea53e0b3ff98fc86ff18a343
tomsrivongrat <42342363+tomsrivongrat@users.noreply.github.com>
2019-09-17 16:24:51 +0700

    Update bot-checktoken.php

--- bot-checktoken.php
+++ bot-checktoken.php
@@ -2,8 +2,8 @@
 
 
 $API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = ''; 
-$channelSecret = '';
+$ACCESS_TOKEN = '/hHNfB0lNpGBkRHYTGWToF0R0Xk9of57Zs+Vd0vbTjJZcg6AgLL2e8BstYfGU8aqjODniNCZZ2FxZ3grkwbPT0InnONJ6AWTZ4Ly+jK+AdRdsQ45bWZNFycCzedGyxfp0gx5rKJaFQTXGJaK2kHCzAdB04t89/1O/w1cDnyilFU='; 
+$channelSecret = '6390e6f7b0953dadffdcba9a287cb2a6';
 
 
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
