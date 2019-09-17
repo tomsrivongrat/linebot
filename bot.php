65e96f9ff399edac5f80be65c34b3ad52e3f5290
Somchai Srivongrat <s.srivongrat@gmail.com>
2019-09-17 16:01:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,8 +2,8 @@
 
 
 $API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = 'ouhqskdRP/sUP8uwpjAadPDJz6rj1Y3IR0/ZznmHBgsPmYq6Q+hzdEJ4OXgyw/8NaLy6GLAZYYbLhF/7S6i8K07k3yxT0sWcMEa6ixgJ2c0XIOEKRfUEQAsHVi4PbQU4HEk9GOq/cmdR3iRkQE9e5gdB04t89/1O/w1cDnyilFU='; 
-$channelSecret = 'ba6e01c3eb0671a32e7d9fb3dbabd67d';
+$ACCESS_TOKEN = 'LFhdswOu1YdhYnhbntxNwtGFR60N7CRO54L7C2BimpG5Gxm08pRzBZesU142eU/xKJDL2roGbK1M7f9oJaB1cC7JCHV1HocgkyzZkKqhbd3RWEcX11MXG1JLIWPQMwpnSYjFve2YyKex0ccSUExVuwdB04t89/1O/w1cDnyilFU=; 
+$channelSecret = '17482501d52e1f82b0100ca4fe5b0cbc';
 
 
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
